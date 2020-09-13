930e77b996a14becdc3f18a9e7d894a93806461b
XSkeolan <61992953+XSkeolan@users.noreply.github.com>
2020-09-14 01:35:17 +0300

    Add files via upload

---
+++ CoffeeMachine/Automatic.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CoffeeMachine
+{
+    internal class Automatic : IAutomatic
+    {
+        public Drinks CurrentDrink
+        {
+            get => default(int);
+        }
+
+        public int CurrentMoney
+        {
+            get => default(int);
+        }
+
+        public int LevelSugar { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public string Instruction => throw new NotImplementedException();
+
+        //Нужно выбрать
+        public IDrink[] Drinks { get => throw new NotImplementedException();}
+
+        public void Make(IDrink drink)
+        {
+            DrinkBeginMade?.Invoke(this, new EventArgs());
+            throw new System.NotImplementedException();
+        }
+
+        public int ReturnСhange()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public event System.EventHandler DrinkBeginMade;
+        public event System.EventHandler DrinkCancelMade;
+    }
+}
